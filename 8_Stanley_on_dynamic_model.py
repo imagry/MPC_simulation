fixed jacobians to forward axe model

8_Stanley_on_dynamic_model.py
@@ -89,7 +89,7 @@ if simulation_params['animate']:
     MPC_cost_axis.grid(True)
     MPC_cost_axis.set_ylabel('MPC cost [unitless]')
 
-animation_dt = 0.1
+animation_dt = 0.05
 ndt = int(animation_dt/simulation_params['dt'])
 pbar = tqdm(total=len(t))
 s = 0
@@ -132,7 +132,7 @@ while not stop_condition:
     if np.mod(i, ndt) == 0 and simulation_params['animate']:
         plt.cla()
         vehicle_animation_axis.clear()
-        vehicle_animation_axis.set_title('t = ' + str(ti) + ' [sec]')
+        vehicle_animation_axis.set_title('t = ' + str("%.2f" % ti) + ' [sec]')
         vehicle_animation_axis.plot(traj_spline_x, traj_spline_y, color='gray', linewidth=2.0)
         # vehicle_traj_line.remove()
         vehicle_traj_line = vehicle_animation_axis.plot(x, y, linewidth=2.0, color='darkviolet')

Classes.py
@@ -334,7 +334,7 @@ class MPC_params:
     # System config
     NX = 4  # state vector: z = [x, y, v, phi]
     NU = 2  # input vector: u = [acceleration, steer]
-    T = 10  # finite time horizon length
+    T = 5  # finite time horizon length
 
     # MPC config
     Q = np.diag([1.0, 1.0, 1.0, 1.0])  # penalty for states
@@ -350,8 +350,8 @@ class MPC_params:
     N_IND = 10  # search index number
     dt = 0.1  # time step
     d_dist = 1.0  # dist step
-    d_a_res = 0.001  # threshold for stopping iteration
-    d_delta_res = 0.001  # threshold for stopping iteration
+    d_a_res = 0.01  # threshold for stopping iteration
+    d_delta_res = 0.01  # threshold for stopping iteration
 
     # vehicle config
     RF = 3.3  # [m] distance from rear to vehicle front end of vehicle
@@ -578,6 +578,32 @@ class MPC:
         return z_bar
     @staticmethod
     def calc_linear_discrete_model(v, phi, delta, P):
+        """
+        calc linear and discrete time dynamic model.
+        :param v: speed: v_bar
+        :param phi: angle of vehicle: phi_bar
+        :param delta: steering angle: delta_bar
+        :return: A, B, C
+        """
+
+        A = np.array([[1.0, 0.0, P.dt * math.cos(phi + delta) , - P.dt * v * math.sin(phi + delta)],
+                      [0.0, 1.0, P.dt * math.sin(phi + delta) ,   P.dt * v * math.cos(phi + delta)],
+                      [0.0, 0.0, 1.0                          , 0.0                               ],
+                      [0.0, 0.0, P.dt * math.sin(delta) / P.WB,1.0                               ]])
+
+        B = np.array([[0.0 , - P.dt * v * math.sin(phi + delta)],
+                      [0.0 ,   P.dt * v * math.cos(phi + delta)],
+                      [P.dt, 0.0                               ],
+                      [0.0 , P.dt * v  * math.cos(delta) / P.WB]])
+
+        C = np.array([P.dt * v * math.sin(phi + delta) * (phi + delta),
+                      - P.dt * v * math.cos(phi + delta) * (phi + delta),
+                      0.0,
+                      -P.dt * v * delta * math.cos(delta) / P.WB])
+
+        return A, B, C
+    @staticmethod
+    def calc_linear_discrete_model_org(v, phi, delta, P):
         """
         calc linear and discrete time dynamic model.
         :param v: speed: v_bar

Functions.py
@@ -149,7 +149,7 @@ def calc_desired_path(scenario, ds=0.1, traj_noise=None, plot_results=False):
         ay = [0.0, 40.0, 15.0, 30.0, 0.0]
         traj_spline_x, traj_spline_y, traj_spline_psi, traj_spline_cur, _ = cs.calc_spline_course(ax, ay, ds=ds)
     elif scenario == 'straight_line':
-        x_range = 100.0
+        x_range = 25.0
         initial_error = 1.0
         traj_samples_x = np.arange(0, x_range, 0.5)
         traj_samples_y = initial_error * np.ones(traj_samples_x.shape)
@@ -180,7 +180,7 @@ def calc_desired_path(scenario, ds=0.1, traj_noise=None, plot_results=False):
         traj_samples_y = np.hstack([traj_samples_y, SF * np.arange(0, l2, dl)])
         traj_spline_x, traj_spline_y, traj_spline_psi, traj_spline_cur, _ = cs.calc_spline_course(traj_samples_x, traj_samples_y, ds=ds)
     elif scenario == 'random_curvature':
-        s = np.arange(0, 500, ds)
+        s = np.arange(0, 250, ds)
         k = 1.0 * (np.random.rand(s.shape[0])-0.5)#.cumsum()
         traj_spline_x, traj_spline_y = calculate_curve_from_curvature(s, k, plot_res=False)
         dx = np.diff(traj_spline_x)

one_inferece_analysis.py
@@ -22,7 +22,7 @@ vehicle_obj = VehicleKinemaicModel(x=traj_spline_x[0] + error_x, y=traj_spline_y
                                    vehicle_params=copy(vehicle_params), simulation_params=copy(simulation_params),
                                    steering_uncertainty_factor=1.0, lr_uncertainty_factor=1.0, WB_uncertainty_factor=1.0)
 t = np.arange(0, simulation_params['t_end'], simulation_params['dt'])
-vehicle_obj.vx = simulation_params['velocity_KPH'] / 3.6 * 0
+vehicle_obj.vx = simulation_params['velocity_KPH'] / 3.6
 # stanly gain
 Ks = 1.0
 SC = StanleyController(Ks=Ks, desired_traj_x=traj_spline_x, desired_traj_y=traj_spline_y, desired_traj_psi=traj_spline_psi)
@@ -39,10 +39,40 @@ x_ref = z_ref[0, :]
 y_ref = z_ref[1, :]
 v_ref = z_ref[2, :]
 psi_ref = z_ref[3, :]
-# vehicle_obj.update(a=MPC_obj.a_exc, delta=MPC_obj.delta_exc)
+if False:
+    # check linearization
+    psi0 = 0.0
+    x0 = 0
+    y0 = 1
+    v0 = 10
+    # z_k = np.array([vehicle_obj.x, vehicle_obj.y, vehicle_obj.vx, vehicle_obj.psi])
+    z_k = np.array([x0, y0, v0, psi0])
+    dt = 0.1
+    a = 0.25
+    delta = 0.4
+    MPC_obj.MPC_params.dt = dt
+    A, B, C = MPC.calc_linear_discrete_model(z_k[2], z_k[3], MPC_obj.delta_exc, MPC_obj.MPC_params)
+    Aorg, Borg, Corg = MPC.calc_linear_discrete_model_org(z_k[2], z_k[3], MPC_obj.delta_exc, MPC_obj.MPC_params)
+    # u = np.array([MPC_obj.a_exc, MPC_obj.delta_exc])
+    u = [a, delta]
+    z_kp1_lin = A @ z_k + B @ u + C
+    z_kp1_lin_org = Aorg @ z_k + Borg @ u + Corg
+    print("z_kp1_lin = " + str(z_kp1_lin))
+    print("z_kp1_lin_org = " + str(z_kp1_lin_org))
+    vehicle_obj.x = z_k[0]
+    vehicle_obj.y = z_k[1]
+    vehicle_obj.vx = z_k[2]
+    vehicle_obj.psi = z_k[3]
+    vehicle_obj.simulation_params["dt"] = dt
+    # vehicle_obj.update(a=MPC_obj.a_exc, delta=MPC_obj.delta_exc)
+    vehicle_obj.update(a=a, delta=delta)
+    z_kp1 = np.array([vehicle_obj.x, vehicle_obj.y, vehicle_obj.vx, vehicle_obj.psi])
+    dz = z_kp1 - z_k
+    print("z_kp1 = " + str(z_kp1))
 
 
 
+# exit()
 animation_figure = plt.figure()
 vehicle_animation_axis = plt.subplot(1, 1, 1)
 ref_traj_line = vehicle_animation_axis.plot(traj_spline_x, traj_spline_y, color='gray', linewidth=2.0)
@@ -52,8 +82,8 @@ vehicle_animation_axis.grid(True)
 vehicle_animation_axis.set_xlabel('x [m]')
 vehicle_animation_axis.set_ylabel('y [m]')
 vehicle_animation_axis.scatter(MPC_obj.x_opt, MPC_obj.y_opt, s=10)
-vehicle_animation_axis.set_xlim(vehicle_obj.x - 25, vehicle_obj.x + 25)
-vehicle_animation_axis.set_ylim(vehicle_obj.y - 25, vehicle_obj.y + 25)
+# vehicle_animation_axis.set_xlim(vehicle_obj.x - 25, vehicle_obj.x + 25)
+# vehicle_animation_axis.set_ylim(vehicle_obj.y - 25, vehicle_obj.y + 25)
 vehicle_animation_axis.grid(True)
 
 plt.figure('state')